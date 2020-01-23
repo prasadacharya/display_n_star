92627bc4b7cca488ca650db80fc38edcc9d46b19
prasadacharya <prasada@mail.usf.edu>
2020-01-22 23:41:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace display_n_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input number");
+
+            string N = Console.ReadLine();
+
+            int n = Convert.ToInt32(N);
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
